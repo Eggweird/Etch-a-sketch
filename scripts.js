9d3c9023f0a75a5fca068a1ce0f31e8c87e3ef0e
Edward Kim <edwardkim@Edwards-MacBook-Pro.local>
2023-01-16 21:19:23 -1000

    Refactoring of code

--- scripts.js
+++ scripts.js
@@ -4,6 +4,7 @@ let mouseDown = false;
 document.body.onmousedown = () => (mouseDown = true);
 document.body.onmouseup = () => (mouseDown = false);
 
+//removes child node, in this case iniitial grid 16x16
 function removeAllChildNodes(parent) {
   while (parent.firstChild) {
     parent.removeChild(parent.firstChild);
@@ -11,73 +12,54 @@ function removeAllChildNodes(parent) {
 }
 
 const grid = document.querySelector(".grid-container");
-//current function allows the grid to be 16x16
-function createGrid() {
-  //this for loop goes through 256 since 16x16 is256
-  //allowing to create small squares that will fill out
-  //the empty spaces and when mouseover event is done
-  //the squares itself will shade in black
-  for (let i = 0; i < 256; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("cell");
-
-    //this chunk of code makes the event listener become
-    //sort of like a click and hold function
-    gridDiv.addEventListener("mouseover", function (e) {
-      if (e.type === "mouseover" && !mouseDown) return;
-      e.target.style.backgroundColor = "black";
-    });
-    gridDiv.addEventListener("mousedown", function (e) {
-      if (e.type === "mouseover" && !mouseDown) return;
-      e.target.style.backgroundColor = "black";
-    });
-
-    grid.appendChild(gridDiv);
+//Initial grid creation of 16x16 when first time opening
+function createGrid(val) {
+  for (let i = 0; i < val * val; i++) {
+    draw();
   }
 }
+
 const clearButton = document.querySelector("#clear-button");
-//This event listener will once clicked traverse through the entire
-//16x16 grid and will color each cells to white, basically clearing
-//the current sketch
+//Button event that will clear the entire grid
 clearButton.addEventListener("click", () => {
   let cells = grid.children;
   let val = document.getElementById("sliderId").value;
-  //for loop will traverse entire 16x16 grid. it will then
-  //get the grid.children for cells. Having each cell
-  //be iterated through and coloring it white
   for (let i = 0; i < val * val; i++) {
     cells[i].style.backgroundColor = "white";
   }
 });
 
+//Draw function that will allow user to draw via mouse down and mouse over
+function draw() {
+  const gridDiv = document.createElement("div");
+  gridDiv.classList.add("cell");
+  //this chunk of code makes the event listener become
+  //sort of like a click and hold function
+  gridDiv.addEventListener("mouseover", function (e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    e.target.style.backgroundColor = "black";
+  });
+  gridDiv.addEventListener("mousedown", function (e) {
+    if (e.type === "mouseover" && !mouseDown) return;
+    e.target.style.backgroundColor = "black";
+  });
+  grid.appendChild(gridDiv);
+}
+
 const slider = document.querySelector(".slider");
 const sliderVal = document.querySelector(".slider-value");
 
+//Slider that will populate the grid with its value chosen val x val
 slider.addEventListener("input", function () {
   //have to use element by ID to get value . element by class name gives an
   //array so can not get value
   let val = document.getElementById("sliderId").value;
-  console.log(val);
   sliderVal.innerHTML = `${val} x ${val}`;
   removeAllChildNodes(grid);
   grid.style.gridTemplateRows = `repeat(${val}, 1fr)`;
   grid.style.gridTemplateColumns = `repeat(${val}, 1fr)`;
-  for (let i = 0; i < val * val; i++) {
-    const gridDiv = document.createElement("div");
-    gridDiv.classList.add("cell");
-
-    //this chunk of code makes the event listener become
-    //sort of like a click and hold function
-    gridDiv.addEventListener("mouseover", function (e) {
-      if (e.type === "mouseover" && !mouseDown) return;
-      e.target.style.backgroundColor = "black";
-    });
-    gridDiv.addEventListener("mousedown", function (e) {
-      if (e.type === "mouseover" && !mouseDown) return;
-      e.target.style.backgroundColor = "black";
-    });
-
-    grid.appendChild(gridDiv);
-  }
+  createGrid(val);
 });
-createGrid();
+
+//Initial grid creation 16x16
+createGrid(16);
